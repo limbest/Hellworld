3e674e457360bc1563bb010e326d54cc058506cd
limhojin <limhojin123@naver.com>
2021-06-05 08:55:07 +0900

    init

---
+++ mytest2/src/main/java/com/gotcoder/mytest2/Mytest2Application.java
@@ -0,0 +1,13 @@
+package com.gotcoder.mytest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mytest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mytest2Application.class, args);
+	}
+
+}
---
+++ mytest2/src/test/java/com/gotcoder/mytest2/Mytest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotcoder.mytest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mytest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
